58d2cfe81ef100f08df8ce946c829fa43c848a81
ducvd <vanduyduc@gmail.com>
2019-01-05 23:27:05 +0700

    All new law & objects

--- src/DynamicSoccer/app/fundamental/impl/soccer/CFinishingUserCase.cpp
+++ src/DynamicSoccer/app/fundamental/impl/soccer/CFinishingUserCase.cpp
@@ -13,9 +13,5 @@ int CFinishingUserCase::run()
 
 int CFinishingUserCase::initialize(IPlayer* player1, IPlayer* player2)
 {
-    player1->positions();
-    player2->positions();
-    player1->positions();
-
     return 0;
 }
