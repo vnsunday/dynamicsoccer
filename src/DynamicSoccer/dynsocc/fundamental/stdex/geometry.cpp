3295eee5a5674f7e485833df18ca597d502f6684
ducvd <ducvd@vislsoft.com>
2020-12-10 21:26:49 +0700

    work

--- src/DynamicSoccer/dynsocc/fundamental/stdex/geometry.cpp
+++ src/DynamicSoccer/dynsocc/fundamental/stdex/geometry.cpp
@@ -1,6 +1,9 @@
+#include "stdex.h"
 #include "geometry.h"
+#include <vector>
 
 using namespace dynsocc;
+using namespace std;
 
 bool GeometryUtil::intersect(Rectangle rc1, Rectangle rc2, Rectangle& rc)
 {
@@ -21,11 +24,45 @@ bool GeometryUtil::intersect(Rectangle rc1, Rectangle rc2)
                     rc1.y + rc1.h >= rc2.y);
 }
 
-Rectangle GeometryUtil::move(Rectangle rc, int offsetX, int offsetY)
+Rectangle GeometryUtil::move(Rectangle r, int offsetX, int offsetY)
 {
-    Rectangle rc2 = rc;
-    rc2.x += offsetX;
-    rc2.y += offsetY;
+    Rectangle r1 = r;
+    r1.x += offsetX;
+    r1.y += offsetY;
 
-    return rc2;
+    return r1;
+}
+
+int GeometryUtil::view_through_lens(
+        Rectangle lens,
+        Rectangle object,
+        Rectangle &view)
+{
+    if (GeometryUtil::intersect(lens, object, view))
+    {
+        return 0;
+    }
+    view = Rectangle(0,0,0,0);
+    return 1;
+}
+
+int GeometryUtil::view_through_lens(
+        std::vector<Rectangle> vlens,
+        Rectangle object,
+        Rectangle &view)
+{
+    Rectangle acc_obj = object;
+    for (int i=vlens.size()-1; i>=0;i--)
+    {
+        int r = view_through_lens(vlens[i],acc_obj,acc_obj);
+
+        if (!STDEX_SUCCESS(r))
+        {
+            view=Rectangle(0,0,0,0);
+            return 1;
+        }
+    }
+
+    view = acc_obj;
+    return 0;
 }
--- src/DynamicSoccer/dynsocc/fundamental/stdex/geometry.h
+++ src/DynamicSoccer/dynsocc/fundamental/stdex/geometry.h
@@ -3,6 +3,7 @@
 
 #include "stdex.h"
 #include "rect.h"
+#include <vector>
 
 namespace dynsocc
 {
@@ -14,7 +15,27 @@ namespace dynsocc
 
             static bool intersect(Rectangle rc1, Rectangle rc2);
 
-            static Rectangle move(Rectangle rc, int offsetX, int offsetY);
+            static Rectangle move(Rectangle r, int offsetX, int offsetY);
+
+            /* 
+            * View through a stack of lens
+            *  vlens[0] is the top <=> the lens that nearest the eye
+            *  
+            *  Eye ==See=>  vlens[0], vlens[1],...,vlens[n] ===> object
+            * 
+            */            
+            static int view_through_lens(
+                Rectangle lens,
+                Rectangle object,
+                Rectangle &view
+            );
+
+            static int view_through_lens(
+                std::vector<Rectangle> vlens,
+                Rectangle object,
+                Rectangle &view
+            );
+
     };
 
 }
