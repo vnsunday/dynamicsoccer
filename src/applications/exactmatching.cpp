0b8b9f05a42b157c6d70303095b8bee6517abd91
vn_sunday <vnsunday@vnsunday.vn>
2026-05-08 10:36:29 +0700

    matching

--- src/applications/exactmatching.cpp
+++ src/applications/exactmatching.cpp
@@ -1,14 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
+#include <fstream>
+#include <string>
+#include <vector>
 const int NMAX = 100;
 int M[NMAX];
 int N[NMAX];
+
+using namespace std;
+/*==================================================
+    CASES
+ *==================================================*/
+int load(const char* file_path, vector<string>& vectorline) {
+    vectorline.clear();
+    std::ifstream file1(file_path);                 
+    if (file1.is_open())                            
+    {                                               
+        std::string line;                           
+        while (std::getline(file1, line))           
+        {                                           
+            vectorline.push_back(line);             
+        }                                           
+        file1.close();                              
+    }                                               
+    return 0;
+}
+
+int analyze(const vector<string>& vline) {
+
+    int l[1000];
+    int r[1000];
+    int Z[1000];
+    string azMatch[1000];
+
+    for (int i=0; i<vline.size(); i++) {
+        int nl = vline[i].length();
+
+        for (int j=1; j<nl; j++) {
+            int k0 = -1;
+            int k1 = j-1;
+
+            while (k1 <= nl && vline[i][++k0] == vline[i][++k1]) {
+            }
+            Z[j] = k1 - j;
+
+            azMatch[j] = vline[i].substr(j, Z[j]);
+        }
+
+        for (int j=1; j<nl; j++) {
+            l[j] = -1;
+            r[j] = -1;
+
+            for (int k=1; k<=j; k++) {
+                if (Z[k] > 0 && r[j] < k + Z[k] - 1) {
+                    r[j] = k + Z[k] - 1;
+                    l[j] = k;
+                }
+            }
+        }
+
+        /*==================================================*
+            Report:
+         *==================================================*/
+        printf("String: %s\r\n", vline[i].c_str());
+        for (int j=1; j<vline[i].length(); j++) {
+            printf("    Z[%d]=%d (%s)\r\n", j, Z[j], azMatch[j].c_str());
+            printf("        [L,R]=[%d,%d]\r\n", l[j], r[j]);
+        }
+    }
+    return 0;
+}
 
 /*==================================================
     Right-2-Left-Scan
  *==================================================*/
 int R2LScan(char* S, int n) {
+    // Right-2-Left 
+    //    * The 1st found= the Right-most (max-index)
+    // Left-2-Right 
+    //    * The 1st found= the left most (minimum index)
     return 0;
 }
 
@@ -21,13 +92,15 @@ int ZTable(char *S, int n, int* Z) {
     int r = -1;
     int lenbeta = 0;
 
-    while ( i < n) {
-        if (i >= r) {
+    while ( i < n ) {
+        if (i > r) {
             j2 = i;
             j1 = 0;
         
+            /* Compare to the prefix */
             while (S[j1] == S[j2] && j2 < n) { j1++; j2++;}
 
+            /* Found */
             if (j2 > i) {
                 l = i;
                 r = j2 - 1;
@@ -92,7 +165,6 @@ int test_ztable(char* S, int n, int* Z) {
     S: String (Length=nS)
     P: Pattern (Length=nP)
  *==================================================*/
-
 // iA: Aligned
 // iS: String Index
 // i:  Comparision
@@ -101,7 +173,6 @@ int test_ztable(char* S, int n, int* Z) {
 int phase(char* S, int nS, char* P, int nP, int i, int iA, int iS) {
     /*========================================
         Exp1. Does logical control flow cover every cases?
-        
      *========================================*/
     if (M[iS] < 0 || M[iS] == N[i] == 0) {
         if (S[iS] == P[i] && i == 0) {
@@ -119,8 +190,12 @@ int phase(char* S, int nS, char* P, int nP, int i, int iA, int iS) {
     return 0;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+
+    vector<string> vline;
+    load("exact.txt", vline);
+    analyze(vline);
+
     const char* S = "abcaabbacaabcaedebbba";
     int n = strlen(S);
     int* Z = (int*)malloc(sizeof(int)*n);
